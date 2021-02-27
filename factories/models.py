1fe0f6f0494f5d9c4d174baed04d5129fa003f4d
Erik Sultanaliev <ersul4ik@gmail.com>
2021-02-27 13:44:11 +0600

    #3 Add DataFixture and DataFixtureColumn models

---
+++ factories/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class DataFixture(models.Model):
+    COMMA = ','
+    SEMICOLON = ';'
+    SEPARATORS = (
+        (COMMA, ','),
+        (SEMICOLON, ','),
+    )
+    SINGLE_QUOTE = "''"
+    DOUBLE_QUOTE = '""'
+    CHARACTERS = (
+        (SINGLE_QUOTE, "''"),
+        (DOUBLE_QUOTE, '""'),
+    )
+    name = models.CharField(max_length=125)
+    column_separator = models.CharField(choices=SEPARATORS, default=COMMA, max_length=5)
+    string_character = models.CharField(choices=CHARACTERS, default=DOUBLE_QUOTE, max_length=5)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Data fixture'
+        verbose_name_plural = 'Data fixtures'
+
+
+class DataFixtureColumn(models.Model):
+    FULL_NAME = 1
+    INTEGER = 2
+    COMPANY = 3
+    ADDRESS = 4
+    SAMPLES = (
+        (FULL_NAME, 'Full name'),
+        (INTEGER, 'Integer'),
+        (COMPANY, 'Company'),
+        (ADDRESS, 'Address'),
+    )
+    fixture = models.ForeignKey('factories.DataFixture', on_delete=models.CASCADE, verbose_name='columns')
+    name = models.CharField(max_length=120)
+    sample = models.PositiveSmallIntegerField(choices=SAMPLES, blank=False)
+    extra = models.JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} {self.name}'
