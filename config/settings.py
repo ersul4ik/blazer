11f5ea480c9ce4a6b598d30116ae105638c7382b
Erik Sultanaliev <ersul4ik@gmail.com>
2021-02-28 17:57:32 +0600

    Allow any host to connect

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env.str('SECRET_KEY', '')
 DEBUG = env.str('DEBUG', False)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['*'])
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
